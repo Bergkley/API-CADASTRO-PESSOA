7d03901a9f87ed98ec6b6ff08a743365c2c8e0bb
Bergkley <bergkley@gmail.com>
2024-07-22 20:12:19 -0300

    feat: criando rota de teste

--- src/router.ts
+++ src/router.ts
@@ -2,5 +2,8 @@ import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } f
 
 export async function routes(fastify: FastifyInstance,options: FastifyPluginOptions){
     
+    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
+     return {ok: true}
+    })
 
 }
