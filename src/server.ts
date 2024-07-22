ad44a38b28239953f367b3fe56595ace65d4ce7a
Bergkley <bergkley@gmail.com>
2024-07-22 20:20:01 -0300

    importação

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import Fastify from "fastify";
 import { routes } from "./router";
+import cors from "@fastify/cors";
 
 const app = Fastify({logger: true});
 
 const start = async () => {
+    await app.register(routes);
     try{
         await app.listen({port:3333,});
     }catch(error){
