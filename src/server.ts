1cf03e8c36eae8e2844069bfbe5750345c3fe45f
Bergkley <bergkley@gmail.com>
2024-07-22 20:09:06 -0300

    feat: criando novo server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,13 @@
 import Fastify from "fastify";
 
+const app = Fastify({logger: true});
+
+const start = async () => {
+    try{
+        await app.listen({port:3333,});
+    }catch(error){
+        process.exit(1);
+    }
+}
+
+start();
