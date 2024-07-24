f429c90540c3d9ba48dc3db90b218cea15c43926
Bergkley <bergkley@gmail.com>
2024-07-23 21:12:36 -0300

    feat: criando interface CreateCustomerProps

--- src/services/CreateCustomerService.ts
+++ src/services/CreateCustomerService.ts
@@ -1,5 +1,10 @@
 import prismaClient from "../prisma";
 
+interface CreateCustomerProps{
+  name: string;
+  email: string;
+}
+
 class CreateCustomerService {
   async execute() {
     console.log("rota foi chamada");
