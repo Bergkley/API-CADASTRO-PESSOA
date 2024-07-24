95060fbd4de173c9a0c2b6e1ec73518e70ec7c3c
Bergkley <bergkley@gmail.com>
2024-07-23 21:28:26 -0300

    feat: criando metodo handle

--- src/controllers/ListCustomerController.ts
+++ src/controllers/ListCustomerController.ts
@@ -3,5 +3,11 @@ import {ListCustomerService} from "../services/ListCustomerService";
 
 class ListCustomerController {
 
-    
+    async handle(request: FastifyRequest, reply: FastifyReply) {
+        const listCustomerService = new ListCustomerService();
+        
+        const customers = await listCustomerService.execute();
+
+        reply.send(customers);
+    }
 }
