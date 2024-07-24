5399f582c15c14f11c9346dab07703d3d8375858
Bergkley <bergkley@gmail.com>
2024-07-23 21:47:28 -0300

    feat: passando id para deletecustomerservice

--- src/controllers/DeleteCustomerController.ts
+++ src/controllers/DeleteCustomerController.ts
@@ -3,6 +3,12 @@ import { DeleteCustomerService } from "../services/DeleteCustomerService";
 
 class DeleteCustomerController {
     async handle(request: FastifyRequest, reply: FastifyReply) {
-        
+        const deleteCustomer = new DeleteCustomerService();
+
+        const { id } = request.query as { id: string };
+
+        const customer = await deleteCustomer.execute({ id });
+
+        reply.send(customer);
     }
 }
