57355c37926036afa42121e14ff9d058ae6439c5
Bergkley <bergkley@gmail.com>
2024-07-23 21:17:10 -0300

    feat: add no banco

--- src/services/CreateCustomerService.ts
+++ src/services/CreateCustomerService.ts
@@ -10,6 +10,14 @@ class CreateCustomerService {
     if (!name || !email) {
       throw new Error("name and email are required");
     }
+    const customer = await prismaClient.customer.create({
+      data: {
+        name,
+        email,
+        status: true
+      },
+    })
+    return customer
   }
 }
 export { CreateCustomerService };
