9daaaccbcce757fddc537a08ea4378ed487f7421
Bergkley <bergkley@gmail.com>
2024-07-26 20:02:00 -0300

    feat: refatoração

--- src/controllers/DeleteCustomerController.ts
+++ src/controllers/DeleteCustomerController.ts
@@ -2,14 +2,14 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { DeleteCustomerService } from "../services/DeleteCustomerService";
 
 class DeleteCustomerController {
-    async handle(request: FastifyRequest, reply: FastifyReply) {
-        const deleteCustomerService = new DeleteCustomerService();
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const deleteCustomerService = new DeleteCustomerService();
 
-        const { id } = request.query as { id: string };
+    const { id } = request.query as { id: string };
 
-        const customer = await deleteCustomerService.execute({ id });
+    const customer = await deleteCustomerService.execute({ id });
 
-        reply.send(customer);
-    }
+    reply.send(customer);
+  }
 }
-export { DeleteCustomerController }
+export { DeleteCustomerController };
