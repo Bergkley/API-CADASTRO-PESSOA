11ed5c6e8259c02f617173fb23029aeb7626b767
Bergkley <bergkley@gmail.com>
2024-07-26 20:02:35 -0300

    feat: refatoração

--- src/services/DeleteCustomerService.ts
+++ src/services/DeleteCustomerService.ts
@@ -1,27 +1,26 @@
 import prismaClient from "../prisma";
 interface DeleteCustomerProps {
-    id: string
+  id: string;
 }
 
 class DeleteCustomerService {
-    async execute({ id }: DeleteCustomerProps) {
-        if(!id) {
-            throw new Error("id is required")
-        }
-        const findCustomer = await prismaClient.customer.findFirst({
-            where: {
-                id:id
-            }
-        })
-
-        if(!findCustomer) {
-            throw new Error("customer not found")
-        }
-
-        await prismaClient.customer.delete({where: {id:findCustomer.id}})
-
-
-        return {message: "customer deleted"}
+  async execute({ id }: DeleteCustomerProps) {
+    if (!id) {
+      throw new Error("id is required");
     }
+    const findCustomer = await prismaClient.customer.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!findCustomer) {
+      throw new Error("customer not found");
+    }
+
+    await prismaClient.customer.delete({ where: { id: findCustomer.id } });
+
+    return { message: "customer deleted" };
+  }
 }
-export { DeleteCustomerService }
+export { DeleteCustomerService };
